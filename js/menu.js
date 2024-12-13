939512d09685854d3b904e205ff37cbf6072e1b8
Bryan Godoy <bry.godoyr@gmail.com>
2024-12-13 17:18:39 -0300

    js

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+var btnMenu = document.getElementById('btn_menu');
+var nav = document.getElementById('nav');
+
+btnMenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+});
+
